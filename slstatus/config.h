ccc243c9d1fe988998509616bf009fd6c3fb8eff
Ganesh Kumar A <ganeshkumarlinux@gmail.com>
2025-06-16 10:59:16 +0530

    fix essid in slstatus

--- slstatus/config.h
+++ slstatus/config.h
@@ -5,7 +5,7 @@ static const char unknown_str[] = "n/a";
 #define MAXLEN 2048
 
 static const struct arg args[] = {
-    { wifi_essid,    " W:%s |", "wlp0s20f3" },
+    { run_command,   " W:%s |", "nmcli -t -f active,ssid dev wifi list | awk -F: '/^yes/ {print $2; exit}'" },
     { run_command,   " %s |",   "amixer get Master | awk -F'[][]' '/%/ { vol=$2; state=$(NF-1) } END { print (state == \"on\" ? \"V+:\" : \"V-:\") vol }'" },
     { battery_state, " B%s:",   "BAT0" },
     { battery_perc,  "%s%% |",  "BAT0" },
